084bebcc5d12e9469c70023f255ca03b8a3988af
Pushkar Srivastava <d.errorfound@gmail.com>
2022-07-30 03:13:48 +0530

    second commit in week7

--- misc/file_reader.py
+++ misc/file_reader.py
@@ -1,3 +1,3 @@
 file = open('/Users/deepaksharma/Documents/python_prac/misc/countries.txt',"a")
-file.write('\nTHis is a new123  line again')
+file.write('\nTHis is a new123456  line again')
 file.close()
